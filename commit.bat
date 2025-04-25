408c10c4a432009c5c98e43f1f0d188ed8e65048
tanglei <16675566723@163.com>
2025-04-25 12:52:46 +0800

    update

--- commit.bat
+++ commit.bat
@@ -1,10 +1,16 @@
+
+echo "设置代理"
+
 git config --global https.proxy 127.0.0.1:7890
 git config --global http.proxy 127.0.0.1:7890
 
+echo "设置代理完成"
+echo "开始提交代码"
 git add .
 git commit -m "update"
 git push
-
-
+echo "提交代码完成"
+echo "取消代理"
 git config --global --unset http.proxy
 git config --global --unset https.proxy
+echo "取消代理完成"
