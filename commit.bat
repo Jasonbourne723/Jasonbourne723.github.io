969a5ecdbe91099ef719211558dc14d676e9a231
tanglei <16675566723@163.com>
2025-04-25 12:51:45 +0800

    update

---
+++ commit.bat
@@ -0,0 +1,10 @@
+git config --global https.proxy 127.0.0.1:7890
+git config --global http.proxy 127.0.0.1:7890
+
+git add .
+git commit -m "update"
+git push
+
+
+git config --global --unset http.proxy
+git config --global --unset https.proxy
